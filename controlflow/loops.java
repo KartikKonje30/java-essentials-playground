82cfcd5387b11166cc54a62731215952966d93f8
KartikKonje30 <kartikkonje.01@gmail.com>
2025-09-12 11:59:14 +0530

    Loops done

--- controlflow/loops.java
+++ controlflow/loops.java
@@ -1,4 +1,46 @@
 
 public class loops {
+
+    public static void main(String[] args){
     
+    // Loops in Java for control flow
+
+    //1. while Loop
+
+    System.out.println("\nWhile Loop Example:");
+    System.out.println("-------------------");
+
+    int num = 1;
+
+    while (num <= 10){
+        System.out.println("(" + num + ") Hello");
+        num++;
+    }
+
+    //2. Do while Loop 
+    // similar to the while loop, but do while loop will atleast run once apart from whatever the condition might be given
+    // first it will run the statement inside do block and then check condition
+
+    System.out.println("\nDo While Loop Example:");
+    System.out.println("------------------------");
+
+    int num2 = 11;
+
+    do{
+        System.out.println("(" + num2 + ") Hello");
+        num2++;
+    }while (num2 <= 10);
+
+
+    //3. For Loop
+
+    System.out.println("\nFor Loop Example:");
+    System.out.println("-------------------");
+
+    for(int i = 0; i<=10; i++){
+        System.out.println("(" + i + ") Hello");
+    }
+
+}
+
 }
